d487f054deb82ebd017ef34f183256bfdbefa775
leeleelee3264 <absinthe4902@naver.com>
2024-06-18 00:06:36 +0900

    ROLLBACK

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,25 @@
 import requests
 import json
 
-NEW_IP_RANGE_FILE = 'ip-ranges.json'
-OLD_IP_RANGE_FILE = 'old-ip-ranges.json'
+NEW_IP_RANGE_FILE = "resources/ipRanges.json"
+OLD_IP_RANGE_FILE = "resources/oldIpRanges.json"
+
+TOP_COMPONENT_FILE = "component/top.js"
+BOTTOM_COMPONENT_FILE = "component/bottom.js"
+BUNDDLE_JS_FILE = "../docs/bundle.js"
+
 
 FETCH_URL = 'https://ip-ranges.amazonaws.com/ip-ranges.json'
 
 
-def move_to_old_ip_range(source_file, destination_file):
+def move_to_old_ip_range() -> None:
     try:
-        with open(source_file, 'r') as file:
+        with open(NEW_IP_RANGE_FILE, 'r') as file:
             data = file.read()
     except FileNotFoundError:
         data = 'File not found this time. Creating dummy old ip range file...'
 
-    with open(destination_file, 'w') as file:
+    with open(OLD_IP_RANGE_FILE, 'w') as file:
         file.write(data)
 
 
@@ -35,17 +40,50 @@ def read_ip_range(filename) -> str:
     return data
 
 
-def is_the_same_ip_range(old_file, new_file) -> bool:
-    old_ip = read_ip_range(old_file)
-    new_ip = read_ip_range(new_file)
+def is_the_same_ip_range() -> bool:
+    old_ip = read_ip_range(OLD_IP_RANGE_FILE)
+    new_ip = read_ip_range(NEW_IP_RANGE_FILE)
 
     return old_ip == new_ip
 
 
+def combine_bundle_js() -> None:
+
+    try:
+        with open(TOP_COMPONENT_FILE, 'r') as file:
+            top_component = file.read()
+
+        with open(NEW_IP_RANGE_FILE, 'r') as file:
+            ip_ranges = json.load(file)['prefixes']
+
+        with open(BOTTOM_COMPONENT_FILE, 'r') as file:
+            bottom_component = file.read()
+
+        with open(BUNDDLE_JS_FILE, 'w') as file:
+            file.write(top_component)
+
+            for ip in ip_ranges:
+                formatted_ip = format_json_to_str(ip)
+                file.write(formatted_ip)
+
+            file.write(bottom_component)
+
+    except IOError as e:
+        print(f"IO error occurred: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+
+def format_json_to_str(json_input: dict) -> str:
+    return (" \"{\\\"ip_prefix\\\": " + "\\\"" + json_input['ip_prefix'] + "\"" + ", \n  \"region\\\": \\\"" + json_input['region'] + "\\\", " +
+            "\\\"service\\\": \\\"" + json_input['service'] + "\\\"   },\" \n")
+
+
 # init
-move_to_old_ip_range(NEW_IP_RANGE_FILE, OLD_IP_RANGE_FILE)
+move_to_old_ip_range()
 fetch_new_ip_range()
 
-# compare to check if the ip ranges are updated.
-t = is_the_same_ip_range(NEW_IP_RANGE_FILE, OLD_IP_RANGE_FILE)
-print(t)
+# check ip is updated.
+# if so, update bundle.js
+if not is_the_same_ip_range():
+    combine_bundle_js()
