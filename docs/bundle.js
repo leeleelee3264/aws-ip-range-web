a19ddee11f05808d77ede15472b26d15fae9354e
leeleelee3264 <absinthe4902@naver.com>
2024-06-20 23:33:10 +0900

    Test format4

--- docs/bundle.js
+++ docs/bundle.js
@@ -64334,7 +64334,7 @@ object-assign
  +"      \"region\": \"ap-northeast-2\", " 
  +"      \"service\": \"AMAZON\", " 
  +"      \"network_border_group\": \"ap-northeast-2\" " 
- +"   }, " 
+ +"   }"
  +"  ]\n" +
 "}");
 
